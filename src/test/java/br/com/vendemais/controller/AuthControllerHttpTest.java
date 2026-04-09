6213ad552106873fe708e4e9526354bee9058225
Jefferson Bezerra <jeffebezerram13@gmail.com>
2026-04-09 16:17:54 -0300

    test: replace deprecated @MockBean with @MockitoBean in tests
    - Update test annotations to use @MockitoBean instead of deprecated @MockBean
    - No structural changes required in test classes

--- src/test/java/br/com/vendemais/controller/AuthControllerHttpTest.java
+++ src/test/java/br/com/vendemais/controller/AuthControllerHttpTest.java
@@ -8,8 +8,8 @@ import br.com.vendemais.support.TestAuthentications;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -28,7 +28,7 @@ class AuthControllerHttpTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private UserRepository userRepository;
 
     @Test
